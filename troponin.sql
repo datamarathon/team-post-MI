bff88cb728cee19249a2147da79929998b136767
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 14:38:08 -0400

    counting up who has nonmissing HADM and ICUSTAY

--- troponin.sql
+++ troponin.sql
@@ -1,5 +1,21 @@
---2747 subjects have a trop over 1 at some point
+--2747 subjects have a trop over 1 at some point                                                                                  
 
+
+select count(*) from ( --9038 trop measurements
 select subject_id, hadm_id, icustay_id, charttime, valuenum
-from "MIMIC2V26"."labevents" 
-where itemid in (50189, 50188) and valuenum > 1 
+from "MIMIC2V26"."labevents"
+where itemid in (50189, 50188) and valuenum > 1
+)
+
+select count(*) from ( --7900 nonmissing hadm
+select subject_id, hadm_id, icustay_id, charttime, valuenum
+from "MIMIC2V26"."labevents"
+where itemid in (50189, 50188) and valuenum > 1 and hadm_id > 1
+)
+
+select count(*) from ( --5519 nonmissing icustay
+select subject_id, hadm_id, icustay_id, charttime, valuenum
+from "MIMIC2V26"."labevents"
+where itemid in (50189, 50188) and valuenum > 1 and icustay_id > 1
+)
+

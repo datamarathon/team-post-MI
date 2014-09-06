5365e48ce62ba165544cbbbf4101249a31810e5c
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 17:41:48 -0400

    nonworking query to side branch

--- pull mortality fields.sql
+++ pull mortality fields.sql
@@ -1,6 +1,14 @@
-select b.*, a.dod, a.hospital_admit_dt, a.icustay_intime, a.expire_flg, a.hospital_expire_flg, 
-a.icustay_first_service, a.gender, a.dob, a.icustay_admit_age
-from "MIMIC2V26"."icustay_detail" as a
+-- NON working query that I (Andy Z.) was trying to form.
+-- Andrew Ward's query works.
+-- This one is basically a dead end.
+
+select b.first_drug, a.*
+from (
+	select distinct subject_id, hadm_id, dod, hospital_admit_dt, icustay_intime, expire_flg, hospital_expire_flg,
+	icustay_first_service, gender, dob, icustay_admit_age
+	from "MIMIC2V26"."icustay_detail"
+) as a
 right join "USER7"."FINAL_PTNTS" as b
 on a.subject_id = b.subject_id and a.hadm_id = b.hadm_id
 order by subject_id
+--what do we do with the following: icustay_intime icustay_first_service icustay_admit_age

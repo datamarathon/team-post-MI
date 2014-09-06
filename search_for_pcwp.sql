1b19825d94171f8e5f1aab8c356f8b265170f3a9
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 12:44:47 -0400

    A commit with wrong filename. Kinda ignore this.

--- search_for_pcwp.sql
+++ search_for_pcwp.sql
@@ -1,2 +1,6 @@
-select * from "MIMIC2V26"."d_chartitems"
-where label like '%PCWP%'
+create table "USER7"."TEST_TABLE" AS (select * from "MIMIC2V26"."d_chartitems"
+where label like '%PCWP%' or label like '%Wedge%' or label like '%Pcwp%' or label like '%wedge%' or label like '%LVEDP%'
+or label like '%LVEDP%'  or label like '%Lvedp%')
+
+
+

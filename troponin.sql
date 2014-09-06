c72473a410f6bb289c8283dc6401108d044d0034
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 14:19:56 -0400

    Full troponin query

--- troponin.sql
+++ troponin.sql
@@ -1,11 +1,5 @@
 --2747 subjects have a trop over 1 at some point
 
-select * from (
-
 select subject_id, hadm_id, icustay_id, charttime, valuenum
 from "MIMIC2V26"."labevents" 
 where itemid in (50189, 50188) and valuenum > 1 
-order by valuenum desc
-)
-
-limit 15

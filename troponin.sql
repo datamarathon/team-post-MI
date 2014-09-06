4342dd1ddd3c9010d4e6f702aafbc5046c65e6f0
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 14:47:16 -0400

    2176 distinct SUB-HAD-ICU have trop over 1

--- troponin.sql
+++ troponin.sql
@@ -19,3 +19,11 @@ from "MIMIC2V26"."labevents"
 where itemid in (50189, 50188) and valuenum > 1 and icustay_id > 1
 )
 
+
+select count(*) from( --2176
+select distinct subject_id, hadm_id, icustay_id from (
+select subject_id, hadm_id, icustay_id, charttime, valuenum
+from "MIMIC2V26"."labevents"
+where itemid in (50189, 50188) and valuenum > 1 and icustay_id > 1
+)
+)

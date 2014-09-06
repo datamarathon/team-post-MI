a8a61e7b33511e5034685ec0abee076aaa103d50
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 17:22:48 -0400

    improved mortality query to include demogs, better ordering, etc

--- pull mortality fields.sql
+++ pull mortality fields.sql
@@ -1,5 +1,5 @@
-select a.dod, a.hospital_admit_dt, a.icustay_intime, a.expire_flg, a.hospital_expire_flg, a.icustay_first_service, b.*
+select b.*, a.dod, a.hospital_admit_dt, a.icustay_intime, a.expire_flg, a.hospital_expire_flg, 
+a.icustay_first_service, a.gender, a.dob, a.icustay_admit_age
 from "MIMIC2V26"."icustay_detail" as a
 right join "USER7"."FINAL_PTNTS" as b
 on a.subject_id = b.subject_id and a.hadm_id = b.hadm_id
-limit 15

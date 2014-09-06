72c20ed6b4443a63a0c30e3ae0010e013a0ba2dd
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-06 12:48:17 -0400

    more accurate SEARCH_FOR_PCWP

--- search_for_pcwp.sql
+++ search_for_pcwp.sql
@@ -1,6 +1,4 @@
-create table "USER7"."TEST_TABLE" AS (select * from "MIMIC2V26"."d_chartitems"
-where label like '%PCWP%' or label like '%Wedge%' or label like '%Pcwp%' or label like '%wedge%' or label like '%LVEDP%'
-or label like '%LVEDP%'  or label like '%Lvedp%')
-
-
+select * from "MIMIC2V26"."d_chartitems"
+where label like '%PCWP%' or label like '%Wedge%' or label like '%Pcwp%' or label like '%wedge%'
+or label like '%LVEDP%'  or label like '%Lvedp%'
 

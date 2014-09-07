07ed5f0e75fe64e0f3a722c52244e581b0b7d10a
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-09-07 09:10:30 -0400

    perl that seems to work

---
+++ stupid_dates.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+use strict;
+
+while(<>){
+    chomp;
+    my @fields = split(/;/);
+    # fields 4 5 7 and 8 are dates
+    print if /SUBJECT/;
+    next if /SUBJECT/;
+    for (4, 5, 7, 8){
+	$fields[$_] =~ s/(.*\d\d\d\d).*/$1/;
+	my $mo = substr($fields[$_], 0, 3);
+	$mo =~ s/(.)/\U$1/g; #upcase
+	my $dt = $fields[$_];
+	$dt =~ s/^... (\d+),.*/$1/;
+	my $yr = $fields[$_];
+	$yr =~ s/.*, (\d\d\d\d).*/$1/;
+	$fields[$_] = $dt . $mo . $yr;
+    }
+    print join(";", @fields) . "\n";
+}

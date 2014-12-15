27afef1de951f5f01dd568705dac2a8331c88ff9
Andy Zimolzak MIMIC on MacBook <andyzimolzak@gmail.com>
2014-12-15 15:49:53 -0500

    more specific

--- va_disch.sql
+++ va_disch.sql
@@ -1,10 +1,9 @@
-select * 
+select category, text
 from mimic2v26.noteevents 
 where (
     category like '%DISCHARGE%'
-    and (text like '%heart fail%'
-        or text like '%CHF%'
-    	or text like '%diures%'	
+    and (text like '%acute decompensated heart%'
+
 	)
 )
 limit 20;
